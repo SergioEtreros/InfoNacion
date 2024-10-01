246ae9a4bd9ecf4cec8c38181a0ee7ba35108190
SergioEtreros <166149027+SergioEtreros@users.noreply.github.com>
2024-10-01 20:04:50 +0200

    agp upgrade

--- build-logic/convention/src/main/java/com/minato/infonacion/KotlinAndroid.kt
+++ build-logic/convention/src/main/java/com/minato/infonacion/KotlinAndroid.kt
@@ -22,14 +22,14 @@ internal fun Project.configureKotlinAndroid(
       }
 
       compileOptions {
-         sourceCompatibility = JavaVersion.VERSION_17
-         targetCompatibility = JavaVersion.VERSION_17
+         sourceCompatibility = JavaVersion.VERSION_21
+         targetCompatibility = JavaVersion.VERSION_21
       }
 
       // Use withType to workaround https://youtrack.jetbrains.com/issue/KT-55947
       tasks.withType<KotlinCompile>().configureEach {
          kotlinOptions {
-            jvmTarget = JavaVersion.VERSION_17.toString()
+            jvmTarget = JavaVersion.VERSION_21.toString()
          }
       }
 
@@ -44,8 +44,8 @@ internal fun Project.configureKotlinAndroid(
 
 internal fun Project.configureKotlinJvm() {
    extensions.configure<JavaPluginExtension> {
-      sourceCompatibility = JavaVersion.VERSION_17
-      targetCompatibility = JavaVersion.VERSION_17
+      sourceCompatibility = JavaVersion.VERSION_21
+      targetCompatibility = JavaVersion.VERSION_21
    }
 
    dependencies {
